611bf223118d061cb5cb295fc2c600498757a761
Shubham Rathi <shubham.rathi@oncehub.com>
2020-01-03 12:10:37 +0530

    api added getLandingPage

--- app.js
+++ app.js
@@ -7,13 +7,11 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,37 @@ var router = express.Router();
 
 const fetch = require('node-fetch');
 
-/* GET home page. */
-router.get('/getLandingPage', async function(req, res, next) {
-  let res1 = await fetch('https://starkproxy.staticso2.com/get-data/GetLandingPageLayout', { method: 'POST',
-  body: JSON.stringify({ linkName: req.query.linkName || '' }),
-  headers: { 'Content-Type': 'application/json' } })
-  let res1_1 = await res1.json();
-  let settingsId = res1_1.settingsId;
+router.post('/getLandingPage', async function (req, res, next) {
+  let res1_1 = await fetch('https://starkproxy.staticso2.com/get-data/GetLandingPageLayout', {
+    method: 'POST',
+    body: JSON.stringify({ linkName: req.body.linkName || '' }),
+    headers: {
+      'Content-Type': 'application/json',
+      "Authorization": "Basic " + Buffer.from("starkqa:starkqa@123").toString("base64")
+    }
+  })
+  let res1_2 = await res1_1.json();
 
-  let res2_1 = await fetch('https://starkproxy.staticso2.com/get-data/GetServiceDetail', { method: 'POST',
-  body: JSON.stringify({ settingsId }),
-  headers: { 'Content-Type': 'application/json' } });
+  let res2_1 = await fetch('https://starkproxy.staticso2.com/get-data/GetSettingsDetail', {
+    method: 'POST',
+    body: JSON.stringify(
+      {
+        "pooledType": res1_2.PooledAvailabilityType,
+        "timeZoneId": 95,
+        "userId": res1_2.userId,
+        "settingsId": res1_2.settingsId,
+        "meetmelinkid": res1_2.meetmeLinkId,
+        "startDate": 1577910600000,
+        "endDate": 1580581799999,
+        "serviceId": res1_2.serviceId || -1,
+        "teamId": res1_2.team
+      }
+    ),
+    headers: {
+      'Content-Type': 'application/json',
+      "Authorization": "Basic " + Buffer.from("starkqa:starkqa@123").toString("base64")
+    }
+  });
   let res2_2 = await res2_1.json();
   let result = res2_2.serviceDetails.map(val => {
     return {
@@ -21,7 +41,7 @@ router.get('/getLandingPage', async function(req, res, next) {
       serviceDuration: val.serviceDuration
     }
   });
-  res.status(200).json(result);
+  return res.status(200).json(result);
 });
 
 module.exports = router;
